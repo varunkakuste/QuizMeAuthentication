3d8ebd2e60eccd6f7316e1a617eae7da0ef1ce7a
bindisevak <bindisevak90@gmail.com>
2015-05-09 14:51:39 -0700

    CMPE275:Bindi_Sevak: Get user details SERVICE method created

--- QuizMeAuthentication/src/main/java/edu/sjsu/authentication/service/IAuthenticationService.java
+++ QuizMeAuthentication/src/main/java/edu/sjsu/authentication/service/IAuthenticationService.java
@@ -3,10 +3,13 @@
  */
 package edu.sjsu.authentication.service;
 
+import edu.sjsu.authentication.UserModel;
+import edu.sjsu.authentication.model.LoginModel;
 
 /**
  * @author Varun
  *
  */
 public interface IAuthenticationService {
+	public UserModel getUserDetails(LoginModel login) throws Exception;
 }
