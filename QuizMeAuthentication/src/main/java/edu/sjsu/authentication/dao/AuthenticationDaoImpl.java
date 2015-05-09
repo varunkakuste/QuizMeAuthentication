9ac36c7939e00dcaf284ac4ed6ea05876b95cc29
bindisevak <bindisevak90@gmail.com>
2015-05-09 14:50:25 -0700

    CMPE275:Bindi_Sevak: Get user details DAO created

--- QuizMeAuthentication/src/main/java/edu/sjsu/authentication/dao/AuthenticationDaoImpl.java
+++ QuizMeAuthentication/src/main/java/edu/sjsu/authentication/dao/AuthenticationDaoImpl.java
@@ -22,5 +22,63 @@ import edu.sjsu.authentication.model.LoginModel;
 @Component
 public class AuthenticationDaoImpl implements IAuthenticationDao {
 	
+	public static final String GET_USER_DETAILS = "SELECT USER_ID, USERNAME, EMAIL, PASSWORD, LASTNAME, FIRSTNAME FROM USERS WHERE USERNAME = ? AND PASSWORD = ?";
+	
+	/**
+	 * dataSource
+	 */
+	private DataSource dataSource;
+	
+	/**
+	 * Default Constructor
+	 */
+	public AuthenticationDaoImpl() {}
+
+	/**
+	 * @param dataSource
+	 */
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	/**
+	 * Method to get user details
+	 */
+	@Override
+	public UserModel getUserDetails(LoginModel login) throws Exception {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		UserModel user = null;		
+		try {
+			connection = (Connection) dataSource.getConnection();
+			preparedStatement = (PreparedStatement) connection.prepareStatement(GET_USER_DETAILS);
+			preparedStatement.setString(1, login.getUserName());
+			preparedStatement.setString(2, login.getPassword());
+			resultSet = preparedStatement.executeQuery();
+			if(resultSet.next()) {
+				user = new UserModel();
+				user.setUserId(resultSet.getInt("USER_ID"));
+				user.setUserName(resultSet.getString("USERNAME"));
+				user.setEmail(resultSet.getString("EMAIL"));
+				user.setPassword(resultSet.getString("PASSWORD"));
+				user.setLastName(resultSet.getString("LASTNAME"));
+				user.setFirstName(resultSet.getString("FIRSTNAME"));
+			}
+		} catch (SQLException sql) {
+			throw new Exception(sql);
+		} catch (Exception exp){
+			throw new Exception(exp);
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sql) {
+					throw new Exception(sql);
+				}
+			}
+		}
+		return user;
+	}
 	
 }
